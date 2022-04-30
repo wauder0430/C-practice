0a50371dad0dce7bbec4922484b97e821337e140
wauder0430 <wlaudaoq123@naver.com>
2022-04-30 17:46:42 +0900

    2022-04-30 practice Cplus

---
+++ BetweenAdder/BetweenAdder.cpp
@@ -0,0 +1,24 @@
+﻿// BetweenAdder.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int val1, val2;
+    int result = 0;
+    std::cout << "두 개의 숫자 입력";
+    std::cin >> val1 >> val2;
+
+    if (val1 < val2) {
+        for (int i = val1 + 1; i < val2; i++)
+            result += i;
+    }
+    else {
+        for (int i = val2 + 1; i < val1; i++)
+            result += i;
+    }
+
+    std::cout << "두 수 사이의 정수 합: " << result << std::endl;
+    return 0;
+}
